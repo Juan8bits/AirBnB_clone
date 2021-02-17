0b0c5a391b2c12f632ee3c9818bce369991e8b5e
Juan8bits <juan160820@gmail.com>
2021-02-17 01:35:08 +0000

    Module init to discover command

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,13 +11,18 @@ class TestBaseModelinit(unittest.TestCase):
     """Class to add Unittest for a BaseModel class.
         __init__ method.
     """
-    pass
+    def test_object(self):
+        obj = BaseModel()
+        self.assertEqual(BaseModel, type(obj))
 
 class TestBaseModelid(unittest.TestCase):
     """Class to add Unittest for a BaseModel class.
         id attribute.
     """
-    pass
+    def test_unique_id(self):
+        ob = BaseModel()
+        ob2 = BaseModel()
+        self.assertNotEqual(ob.id,ob2.id)
 
 if __name__ == '__main__':
     unittest.main()
