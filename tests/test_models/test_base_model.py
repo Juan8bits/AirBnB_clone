c1765d3886af0201f0128ce3ecf7972150b341fc
Juan8bits <juan160820@gmail.com>
2021-02-17 01:17:43 +0000

    Test directory

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unittest for BaseModel class.
+    16/02/2021
+"""
+import os
+import unittest
+from models import storage
+from models.base_model import BaseModel
+
+class TestBaseModelinit(unittest.TestCase):
+    """Class to add Unittest for a BaseModel class.
+        __init__ method.
+    """
+    pass
+
+class TestBaseModelid(unittest.TestCase):
+    """Class to add Unittest for a BaseModel class.
+        id attribute.
+    """
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
