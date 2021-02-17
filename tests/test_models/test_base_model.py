8ed13e8d9a86b8cd55c32c2359b0e930aa787d1a
Juan8bits <juan160820@gmail.com>
2021-02-17 16:59:20 +0000

    Add some test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,11 +4,12 @@
 """
 import os
 import unittest
+import datetime
 from models import storage
 from models.base_model import BaseModel
 
 
-class TestBaseModelinit(unittest.TestCase):
+class TestBaseModel_init(unittest.TestCase):
     """Class to add Unittest for a BaseModel class.
         __init__ method.
     """
@@ -16,15 +17,34 @@ class TestBaseModelinit(unittest.TestCase):
         obj = BaseModel()
         self.assertEqual(BaseModel, type(obj))
 
-
-class TestBaseModelid(unittest.TestCase):
-    """Class to add Unittest for a BaseModel class.
-        id attribute.
-    """
     def test_unique_id(self):
         ob = BaseModel()
         ob2 = BaseModel()
         self.assertNotEqual(ob.id, ob2.id)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_type_id(self):
+        ob = BaseModel()
+        self.assertEqual(str, type(ob.id))
+
+    def test_type_created_at(self):
+        ob = BaseModel()
+        self.assertEqual(datetime.datetime, type(ob.created_at))
+
+    def test_type_update_at(self):
+        ob = BaseModel()
+        self.assertEqual(datetime.datetime, type(ob.updated_at))
+
+    def test_type_to_dict(self):
+        ob = BaseModel()
+        dic = ob.to_dict()
+        self.assertEqual(dict, type(dic))
+
+
+class TestBaseModel_str_repr(unittest.TestCase):
+    """Class to add Unittest for a BaseModel class.
+        str method.
+    """
+    def test_unique_id(self):
+        ob = BaseModel()
+        ob2 = BaseModel()
+        self.assertNotEqual(ob.id, ob2.id)
